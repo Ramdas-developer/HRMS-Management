97f69626a528abdf23f20bc405c253a2ddb44033
ramdas <ramdassinghmt30@gmail.com>
2024-12-30 17:34:16 +0530

    change in home page

--- frontend/src/component/HomePage.jsx
+++ frontend/src/component/HomePage.jsx
@@ -1,16 +1,17 @@
-import React from 'react'
-import {  useNavigate } from 'react-router-dom';
-import '../pages/cssFiles/home.css'
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import "../pages/cssFiles/home.css";
 
 const HomePage = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
   return (
-    <div className='home'>
-        <h1>Welcome to the HRMS Dashboard</h1>
-        <button className='homebtn' onClick={()=>navigate('/login')}>Login</button>
-
+    <div className="home">
+      <h1>Welcome to the HRMS Dashboard pleas continue</h1>
+      <button className="homebtn" onClick={() => navigate("/login")}>
+        Login
+      </button>
     </div>
-  )
-}
+  );
+};
 
 export default HomePage;
